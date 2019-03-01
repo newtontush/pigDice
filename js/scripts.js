c39c30f2c0cb2cac28324a939d2c97bc82cc7eae
newtontush <newtontush@gmail.com>
2019-03-01 14:02:27 +0300

    Add list of elements in index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 function PigDice() {
-    var value1 = document.getElementById("die1");
-    var value2 = document.getElementById("die2");
     
-}
+    var die1 = document.getElementById("die1");
+    var die2 = document.getElementById("die2");
+    var d1 = Math.floor(Math.random() * 6) + 1;
+    var d2 = Math.floor(Math.random() * 6) + 1;
+    die1 = d1;
+    die2 = d2;
+    };
