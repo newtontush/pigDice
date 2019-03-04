f9d7b946f861ec82406b5c0152c4e0516e8e2c35
newtontush <newtontush@gmail.com>
2019-03-04 10:19:06 +0300

    Add elemnts to java script

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,34 @@
-function PigDice() {
-    var value1 = document.getElementById("die1");
-    var value2 = document.getElementById("die2");
-    var die1 = Math.floor(Math.random() * 6) + 1;
-    var die2 = Math.floor(Math.random() * 6) + 1;
-    var sum = die1 + die2;
-    value1 = die1;
-    value2 = die2;
-    if (sum <=10 ) {
-alert("wow");
-    }else {
-        alert("will");
-    };
-    };
+var player1="";
+var player2="";
+var dice = function () {
+    return Math.floor(6*Math.random())+1;
+}
+function player(turn) {
+    this.roll=  0;
+    this.temposcore = 0;
+    this.totalscore = 0;
+    this.turn = turn;
+    this.playerName;
+}
+player.prototype.roll = function() {
+    if(this.roll === 1) {
+        this.temposcore = 0;
+        alert(this.playerName + "'" + "You rolled a 1!!!!");
+    }else{
+        this.temposcore += this.roll;
+    }
+}
+player.prototype.hold = function() {
+    this.totalscore += this.temposcore;
+    this.temposcore = 0;
+    alert(this.playerName + "your turn is over!");
+}
+player.prototype.winner = function() {
+    if(this.totalscore >= 100) {
+        alert(this.playername + "won  this round!");
+    }
+}
+var clearValues = function() {
+    $(.)
+}
+
